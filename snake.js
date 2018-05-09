ef671c301df7969efbd68b080613b4ff1016a2ee
ddickson634 <ddickson634@gmail.com>
2018-05-08 23:29:05 -0400

    made the snake array as well as the random food location.

--- snake.js
+++ snake.js
@@ -1,3 +1,29 @@
-const cvs = document.getElementById('canvas');
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
 
-cvs.getContext('2d');
+//make unit
+const box = 32;
+
+//load images
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+//make snake
+let snake = []
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+}
+
+// create food
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
+//get score
+
+let score = 0;
+
